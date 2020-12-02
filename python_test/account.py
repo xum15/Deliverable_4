5e01bdc79f43c6b60aba493dbddf28c381a52256
xum15 <75153684+xum15@users.noreply.github.com>
2020-12-01 21:47:06 -0500

    Testing account features

--- python_test/account.py
+++ python_test/account.py
@@ -1,13 +1,37 @@
 class passwords:
 
-    def __init__(self, password):
-        self.password = password
-
     # takes a password and returns True if valid, False otherwise
-    def verify_password(self):
+    def verify_password(self, password):
         return None
 
     # changes the password
-    def password_change(self, new_password):
+    def password_change(self, old_password, new_password):
         return None
 
+class accounts(passwords):
+    
+    def __init__(self, username, password, confirm_password, email, name, phone):
+        self.username = username
+        self.password = password
+        self.confirm_password = confirm_password
+        self.email = email
+        self.name = name
+        self.phone = phone
+
+    # Returns True if the information satisfies requirement 3.1.1
+    # Returns False otherwise
+    def verify_account(self):
+        return None
+
+    # Creates account
+    def create_account(self):
+        return None
+
+    # Changes profile in which the user can change any information listed below
+    def change_profile(self, username, email, name, phone):
+        return None
+
+    # Login system where the user enters their username/email and password
+    # Returns true if the login information is correct and false otherwise
+    def login(self, username_or_email, password):
+        return None
--- python_test/test_account.py
+++ python_test/test_account.py
@@ -1,18 +1,78 @@
 from python_test.account import passwords
+from python_test.account import accounts
 
-# Test to verify the valid passwords
+# Test to verify the valid passwords which consists of at least 8 characters
+# It must include an upper case letter, lower case letter and a number
+# Requirement 5.1.3
 def test_verify_valid_password():
-    p_code = passwords("Sabc13579")
-    assert p_code.verify_password() == True, "password should be valid"
+    valid_password = ["Sabc13579", "Bmwac34567", "Aclaw1122"]
+    p_word = passwords()
+    for passcode in valid_password:
+        assert p_word.verify_password(passcode) == True, "password should be valid"
 
 
 # Test to verify the invalid passwords
 def test_verify_invalid_password():
-    p_code = passwords("123456")
-    assert p_code.verify_password() == False, "password should be invalid"
+    invalid_password = ["123456", "abcdef", "McMaster"]
+    p_word = passwords()
+    for passcode in invalid_password:
+        assert p_word.verify_password(passcode) == False, "password should be invalid"
 
 
+# Test to verify if the system changes the old password to the new password for requirement 3.1.3
 def test_change_password():
-    p_code = passwords("S123456mac")
-    new_password = "ABc1234567"
-    assert p_code.password_change(new_password) == "ABc1234567", "password should be changed to the new password"
+    p_code = passwords()
+    old = "Sabc13579"
+    new = "ABc1234567"
+    assert p_code.password_change(old, new) == new, "password should be changed to the new password"
+
+
+# Test to verify the valid accounts with unique account information
+# Account object includes account(username, password, confirm password, email, phone number)
+# Requirement 3.1.1
+def test_valid_unique_account():
+    account_1 = accounts("McMaster123", "Scba123456", "Scba123456", "suofqpd1200@gmail.com", "John Doe", "416-956-8837")
+    account_2 = accounts("McMaster456", "Scba123456", "Scba123456", "wings1122@gmail.com", "Rick Lee", "416-985-8531")
+    account_1.create_account()  # Creates the first account
+    assert account_2.verify_account() == True, "account information should be valid"
+
+
+# Test to verify an invalid account
+# It will be done by testing 2 accounts with the same username
+# Requirement 3.1.1
+def test_invalid_account_1():
+    account_1 = accounts("McMaster123", "Scba123456", "Scba123456", "suofqpd1200@gmail.com", "John Doe", "416-956-8837")
+    account_2 = accounts("McMaster123", "Sabc123456", "Sabc123456", "suofqpd1500@gmail.com", "Rob Doe", "416-988-8815")
+    account_1.create_account()  # Creates the first account
+    assert account_2.verify_account() == False, "account information should be invalid as it's not unique"
+
+
+# Test to verify another invalid account
+# It will be done by testing 1 account with different passwords for confirmation
+# Requirement 3.1.1
+def test_invalid_account_2():
+    account_1 = accounts("McMaster123", "Scba123456", "ABCas123456", "suofqpd1200@gmail.com", "John Doe", "416-956-8837")
+    assert account_1.verify_account() == False, "account information should be invalid as the passwords are different"
+
+
+# Test if the profile is changed by changing the email address
+# Requirement 3.1.3
+def test_change_profile():
+    account_1 = accounts("McMaster456", "Scba123456", "Scba123456", "wings1122@gmail.com", "Rick Lee", "416-985-8531")
+    account_1.change_profile("Mcmaster456", "sdcasfef117@mcmaster.ca", "Rick Lee", "416-985-8531")
+    assert account_1.email == "sdcasfef117@mcmaster.ca", "email should be changed to the new email"
+
+
+# Test if the login system can correctly interpret the username/email and password the user entered
+# Requirement 3.1.2
+# Test a valid login information
+def test_login_authentication_valid():
+    account_1 = accounts("McMaster456", "Scba123456", "Scba123456", "wings1122@gmail.com", "Rick Lee", "416-985-8531")
+    assert account_1.login("McMaster456", "Scba123456") == True, "login information should be correct"
+
+# Test an invalid login information where the password the user entered is incorrect
+def test_login_authentication_invalid():
+    account_1 = accounts("McMaster456", "Scba123456", "Scba123456", "wings1122@gmail.com", "Rick Lee", "416-985-8531")
+    assert account_1.login("McMaster456", "Scale1122") == False, "login information should be incorrect"
+
+
